a621cb6e7d00f3f8af3ec398c8393b94aef156b2
Selmyyyyy <salmasobhi2200@gmail.com>
2026-02-16 12:25:33 +0100

    scrivi quello che manca

--- src/app/Components/header-component/header-component.ts
+++ src/app/Components/header-component/header-component.ts
@@ -12,4 +12,43 @@ import { RouterLink } from "@angular/router";
 })
 export class HeaderComponent {
 
+  paletteId = 0;
+  isDarkMode = false;
+
+
+  switchMode() {
+    // if (this.isDarkMode === true) {
+    //   this.isDarkMode = false;
+    // } else {
+    //   this.isDarkMode = true;
+    // }
+
+    this.isDarkMode = !this.isDarkMode;
+    // è la stessa cosa della parte commentata sopra 
+    document.body.classList.toggle('dark-body');
+    //toggle Serve a scambiare uno stato tra due opzioni(in questo caso dark e light)
+
+
+
+  }
+
+  switchPalette() {
+
+    this.paletteId++;
+    if (this.paletteId > 2) {
+      this.paletteId = 0;
+    }
+    if (this.paletteId === 1) {
+      document.body.classList.add('charteuse-theme');
+    } else if (this.paletteId === 2) {
+      document.body.classList.remove('charteuse-theme');
+      document.body.classList.add('orange-theme');
+
+    } else {
+      document.body.classList.remove('charteuse-theme');
+      document.body.classList.remove('charteuse-theme');
+
+    }
+
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home-component/home-component';
 import { ListComponent } from './Components/list-component/list-component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: "", redirectTo:"/home", pathMatch:"full"},
     {path:"home", component: HomeComponent},
     {path: "list", component: ListComponent},
+    {path:"dashboard", component: DashboardComponent},
     {path: "**", redirectTo: "/home", pathMatch: "full" }
 
  ];  
